fb1c0b0401ab705c04122130dc70d4c8b05c27e5
Sarah Kurisu <sarah.kurisu@gmail.com>
2016-08-25 22:22:14 -0400

    pulling data

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import getFeed from './helpers';
+import ListCardText from './ListCardText';
 
-const ListCard = () => {
-  return (
-    <div className="row">
-      <div className="col-sm-6">
-        <div className="card card-block card-outline-secondary">
-          <h2 className="card-title">ListCard Component</h2>
-          <p className="card-text">At some point data will be here</p>
+
+class ListCard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      everything: []
+    }
+  }
+  componentDidMount(){
+    this.init()
+  }
+  init(){
+    console.log("init");
+    getFeed()
+      .then(function(data) {
+        console.log("data: ",data);
+        this.setState({
+          everything: data
+        })
+      }.bind(this))
+  }
+  render(){
+    var t = JSON.stringify(this.state.everything);
+    return (
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card card-block card-outline-secondary">
+            <h2 className="card-title">ListCard Component</h2>
+            <p className="card-text">{t}</p>
+            {/*<ListCardText text={this.state.everything} />*/}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+
+  }
 }
 
 export default ListCard;
